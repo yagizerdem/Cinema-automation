abe79550fc86a701aacc8a8b838402d49e52d860
yagizerdem <yagizerdem819@gmail.com>
2025-11-08 13:05:28 +0300

    mongo db connection added

---
+++ backend/config/load.dev.env.js
@@ -0,0 +1,5 @@
+const path = require("path");
+const envPath = path.resolve(__dirname, "../", ".dev.env");
+require("dotenv").config({ path: envPath });
+
+module.exports = {};
--- backend/server.js
+++
@@ -1,5 +0,0 @@
-const path = require("path");
-const envPath = path.resolve(__dirname, ".env");
-require("dotenv").config({ path: envPath });
-
-console.log(process.env.PORT);
