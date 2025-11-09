e0bbd456e42cf1351648f6579b69f122b862b9b2
yagizerdem <yagizerdem819@gmail.com>
2025-11-09 12:48:12 +0300

    user model added

---
+++ backend/enum/userRoles.js
@@ -0,0 +1,10 @@
+const userRoles = Object.freeze({
+  ADMIN: "Admin",
+  CINEMA_DEPARTMENT_MANAGER: "CinemaDepartmentManager",
+  BOX_OFFICE_SUPERVISOR: "BoxOfficeSupervisor",
+  BOX_OFFICE_CLERK: "BoxOfficeClerk",
+  IT_MANAGER: "ITManager",
+  CLIENT: "Client",
+});
+
+module.exports = { userRoles };
---
+++ backend/model/entity/User.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const { userRoles } = require("../../enum/userRoles");
+
+const schema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    userRole: {
+      type: String,
+      enum: Object.values(userRoles),
+      default: userRoles.CLIENT,
+    },
+
+    normalizedFirstName: { type: String },
+    normalizedLastName: { type: String },
+    normalizedFullName: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+schema.pre("save", function (next) {
+  this.normalizedFirstName = this.firstName.toLowerCase();
+  this.normalizedLastName = this.lastName.toLowerCase();
+  this.normalizedFullName = `${this.firstName} ${this.lastName}`.toLowerCase();
+
+  next();
+});
+
+const User = mongoose.model("User", schema);
+
+module.exports = { User };
