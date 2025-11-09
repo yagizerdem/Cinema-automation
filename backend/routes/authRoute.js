5ddb2420f8a097eb759e69380908137930bd4680
yagizerdem <yagizerdem819@gmail.com>
2025-11-09 13:08:27 +0300

    async wrapper util added

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -2,7 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { register } = require("../controller/authController");
 const { ensureNotEmptyBody } = require("../middleware/ensureNotEmptyBody");
+const { asyncWrapper } = require("../utils/asyncWrapper");
 
-router.post("/register", ensureNotEmptyBody, register);
+router.post("/register", ensureNotEmptyBody, asyncWrapper(register));
 
 module.exports = { router };
