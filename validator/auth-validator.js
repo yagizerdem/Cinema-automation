fc46c1117bb59fda6d8b661d85225034c484dcf9
yagiz erdem <2yagiz004@gmail.com>
2026-06-02 21:33:31 +0300

    some utils added

---
+++ validator/auth-validator.js
@@ -0,0 +1,37 @@
+// validators/authValidator.js
+const Joi = require("joi");
+
+function getRegisterValidator() {
+  const registerValidator = Joi.object({
+    firstName: Joi.string().trim().min(2).max(50).required().messages({
+      "string.empty": "First name is required",
+      "string.min": "First name must be at least 2 characters",
+      "string.max": "First name must be at most 50 characters",
+      "any.required": "First name is required",
+    }),
+
+    lastName: Joi.string().trim().min(2).max(50).required().messages({
+      "string.empty": "Last name is required",
+      "string.min": "Last name must be at least 2 characters",
+      "string.max": "Last name must be at most 50 characters",
+      "any.required": "Last name is required",
+    }),
+
+    email: Joi.string().trim().lowercase().email().required().messages({
+      "string.empty": "Email is required",
+      "string.email": "Email must be valid",
+      "any.required": "Email is required",
+    }),
+
+    password: Joi.string().min(6).max(64).required().messages({
+      "string.empty": "Password is required",
+      "string.min": "Password must be at least 6 characters",
+      "string.max": "Password must be at most 64 characters",
+      "any.required": "Password is required",
+    }),
+  });
+
+  return registerValidator;
+}
+
+module.exports = { getRegisterValidator };
